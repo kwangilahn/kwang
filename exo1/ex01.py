ed79e91aa7d1ff68a3667dfe5dfd2fa630ee80e1
kwangil ahn <anki3485@naver.com>
2016-03-15 12:57:12 +0900

    ex01.py 내용추가

--- exo1/ex01.py
+++ exo1/ex01.py
@@ -1,1 +1,7 @@
 print 'Hello World'
+print 'Hello Again'
+print "I like typing this"
+print "This is fun"
+print "Yoy!printing"
+print " I'd much rather you 'not'."
+print 'I."saind" do not touch this.'
